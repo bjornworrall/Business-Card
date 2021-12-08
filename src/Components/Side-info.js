58ab56f58008b71849e6c2b89a0db13146ba84be
bjorn worrall <bjornworrall@gmail.com>
2021-12-08 19:25:43 +0200

    fixed br

--- src/Components/Side-info.js
+++ src/Components/Side-info.js
@@ -9,7 +9,7 @@ export default function sideInfo() {
             <div class="description">
                 <h3>Description</h3>
                 <p>This is a personal dashboard i have created and have loaded onto google chrome, so whenever a new tab is opened. this dashboard will appear, using Apis, i have the Cryptocurrency, Weather and news Apis updating consistently</p>
-                <br>
+                <br/>
                 <a href="https://codepen.io/bjorn-worrall/pen/PoJZOoP">My Codepen</a>
             </div>
              <div class="languages">
