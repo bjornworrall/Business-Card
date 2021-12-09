c2970a995fc145bddfe041924392321c57df6284
bjorn worrall <bjornworrall@gmail.com>
2021-12-09 10:34:54 +0200

    fixed layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@
 import React from "react";
 import Header from "./Components/Header";
 import MainContent from "./Components/MainContent"
-import Footer from "./Components/Footer"
+import Footer from "./Components/Footer";
+import Description from "./Components/Description";
 
 
 
 export default function App() {
   return (
-    <div className="container">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+  <div className="whole-page">
+        <Description  />
+        <div className="container">
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+  </div>
+    
   )
 }
---
+++ src/Components/Description.js
@@ -0,0 +1,29 @@
+import React from "react" 
+
+
+export default function Description() {
+    return (
+        <section className="profile-description">
+        <h1>Business Card</h1>
+        <div class="info-header">
+            <div class="description">
+                <h3>Description</h3>
+                <p>By using the basics of React i created a business card, creating different components and compiling it all together</p>
+                <br/>
+                <a href="https://github.com/bjornworrall/Business-Card">My Code on Github</a>
+            </div>
+             <div class="languages">
+                <h4>Components used</h4>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+        
+        
+    </section>
+    )
+}
